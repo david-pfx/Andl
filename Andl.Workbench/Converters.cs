19b6730edd98baa877fe0aaf623a178915eb7d92
david <david@andl.org>
2016-01-27 00:21:08 +1100

    Add a converter. More Nuget updates.

---
+++ Andl.Workbench/Converters.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace Andl.Workbench {
+  public class TitleConverter : IMultiValueConverter {
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture) {
+      var filename = values[0] as string ?? "";
+      var modified = values[1] as bool? ?? false;
+      var title = "Andl Workbench";
+      if (filename != "") title += " - " + filename + (modified ? " *" : "");
+      return title;
+    }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) {
+      return null;
+    }
+  }
+}
