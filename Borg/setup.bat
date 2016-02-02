b82ecc662903cc5eccfe884f9834111d01688c6e
david <david@andl.org>
2016-02-02 18:50:18 +1100

    Delete files for internal use only

--- Borg/setup.bat
+++
@@ -1,6 +0,0 @@
-echo Setting up Borg database and catalog
-
-for /d %%f in (*.sandl) do rd %f /s
-..\debug\andl borg.andl borg /t
-
-..\bin\thrift-0.9.2.exe --gen java borg.thrift
--- Work/runsql.bat
+++
@@ -1,6 +0,0 @@
-: Run prog and capture SQL
-
-  ..\andl.Compiler\bin\debug\andl.compiler /x %1 |! gawk "/Execute: (.*)/ { $1=\"\";print $0 }" >sql.txt
-
-c:\sw\sqlite\sqlite3 -init sql.txt
-
