b8db1c8a46a9650f450d4cd2ca4162b4bff47cec
david <david@andl.org>
2016-02-07 16:16:13 +1100

    Update release scripts

--- build-rls.bat
+++ build-rls.bat
@@ -1,15 +1,26 @@
 : build Andl release
-setlocal
-set rlsdir=temp-andl-rls
 
-rd %rlsdir% /s
-if exist %rlsdir% echo "needs manual delete"
-if exist %rlsdir% exit /b
+@if "%1"="" echo"Usage: build-rls <dirpath>
+@if "%1"="" exit /b
 
-git clone Andl %rlsdir% -b master
-cd %rlsdir%
+@setlocal
+@set rlsdir=%1
+
+if not exist %rlsdir% git clone Andl %rlsdir% -b master
+@cd %rlsdir%
 
 c:\sw\nuget restore Andl-master.sln
 
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\common7\tools\vsvars32.bat"
 msbuild Andl-master.sln /t:Clean;Build /p:Configuration=Release
+
+pushd ThriftTest
+call setup
+popd
+pushd ThriftPartSupplier
+call setup
+popd
+
+msbuild Andl-master.sln /t:Build /p:Configuration=Release
+
+echo"Run zip-rls to create upload.
--- zip-rls.bat
+++ zip-rls.bat
@@ -2,6 +2,8 @@
 
 setlocal
 set rlsver=v10b2
-set rlsdate=16f204
-del andl-%rlsver%-%rlsdate%.zip 
-zip andl-%rlsver%-%rlsdate%.zip readme.txt /r bin /r sample /r test /r thrift
+for /f %%i in ('powershell get-date -format "{yyMMdd}"') do set rlsdate=%%i 
+set zname=andl-%rlsver%-%rlsdate%.zip
+
+if exist %zname% del %zname% 
+zip %zname% readme.txt /r bin /r sample /r test /r thrift
