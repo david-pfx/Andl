09f00a3ea507703a21529b4f480dc9f038bed99d
david <david@andl.org>
2016-02-17 23:46:02 +1100

    Update solution files

--- zip-rls.bat
+++ zip-rls.bat
@@ -1,9 +1,12 @@
 : zip-rls.bat -- zip release files into upload
 
+@if "%1"="" echo"Usage: zip-rls <dirpath>
+@if "%1"="" exit /b
+
 setlocal
 set rlsver=v10b2
 for /f %%i in ('powershell get-date -format "{yyMMdd}"') do set rlsdate=%%i 
 set zname=andl-%rlsver%-%rlsdate%.zip
 
 if exist %zname% del %zname% 
-zip %zname% readme.txt /r bin /r sample /r test /r thrift
+zip %zname% %1\readme.txt /r %1\bin /r %1\sample /r %1\test /r %1\thrift
